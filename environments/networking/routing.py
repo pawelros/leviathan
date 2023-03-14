e083128fad7d566a50f1f943a560366dfe3695d7
Paweł Rosiński <dev.rosiv@gmail.com>
2023-03-14 17:22:03 +0100

    Add nat gateway to the networking account

--- environments/networking/routing.py
+++ environments/networking/routing.py
@@ -19,35 +19,92 @@ class Routing(ComponentResource):
         self.internet_gateway = aws.ec2.InternetGateway(
             "networking-internet-gateway",
             vpc_id=vpc.id,
+            tags={"Name": "networking-internet-gateway"},
             opts=child_opts
         )
 
         # Route tables
         self.public_route_table = aws.ec2.RouteTable(
-            'networking-public-rt',
+            "networking-public-rt",
             vpc_id=vpc.id,
-            opts=pulumi.ResourceOptions(parent=self.internet_gateway, providers=child_opts.providers),
+            tags={"Name": "networking-public-route-table"},
+            opts=pulumi.ResourceOptions(
+                parent=self.internet_gateway, providers=child_opts.providers
+            ),
         )
 
         aws.ec2.Route(
-            'networking-internet-route',
+            "networking-internet-route",
             gateway_id=self.internet_gateway.id,
             route_table_id=self.public_route_table.id,
             destination_cidr_block=cidrs.EVERYWHERE,
-            opts=pulumi.ResourceOptions(parent=self.public_route_table, providers=child_opts.providers),
+            opts=pulumi.ResourceOptions(
+                parent=self.public_route_table, providers=child_opts.providers
+            ),
         )
 
         aws.ec2.MainRouteTableAssociation(
-            'networking-public-main-route-association',
+            "networking-public-main-route-association",
             route_table_id=self.public_route_table.id,
             vpc_id=vpc.id,
-            opts=pulumi.ResourceOptions(parent=self.public_route_table, providers=child_opts.providers),
+            opts=pulumi.ResourceOptions(
+                parent=self.public_route_table, providers=child_opts.providers
+            ),
         )
 
         for index, subnet in enumerate(vpc.public_subnets):
             aws.ec2.RouteTableAssociation(
-                f'networking-public-route-association-{index}',
+                f"networking-public-route-association-{index}",
                 route_table_id=self.public_route_table.id,
                 subnet_id=subnet,
-                opts=pulumi.ResourceOptions(parent=self.public_route_table, providers=child_opts.providers),
+                opts=pulumi.ResourceOptions(
+                    parent=self.public_route_table, providers=child_opts.providers
+                ),
+            )
+
+        # NAT Gateway
+        eip = aws.ec2.Eip(
+            "networking-nat-eip",
+            vpc=True,
+            opts=pulumi.ResourceOptions(
+                parent=self.public_route_table, providers=child_opts.providers
+            ),
+        )
+
+        nat_gateway = aws.ec2.NatGateway(
+            "networking-nat-gw",
+            allocation_id=eip.id,
+            subnet_id=vpc.public_subnets[0],
+            tags={"Name": "networking-nat-gw"},
+            opts=pulumi.ResourceOptions(
+                parent=self.public_route_table, providers=child_opts.providers
+            ),
+        )
+
+        # Private route table
+        private_route_table = aws.ec2.RouteTable(
+            "networking-private-route-table",
+            vpc_id=vpc.id,
+            tags={"Name": "networking-private-route-table"},
+            opts=pulumi.ResourceOptions(parent=vpc, providers=child_opts.providers),
+        )
+
+        private_route = aws.ec2.Route(
+            "networking-private-route",
+            nat_gateway_id=nat_gateway.id,
+            route_table_id=private_route_table.id,
+            destination_cidr_block=cidrs.EVERYWHERE,
+            opts=pulumi.ResourceOptions(
+                parent=nat_gateway, providers=child_opts.providers
+            ),
+        )
+
+        for index, subnet in enumerate(vpc.private_subnets):
+            aws.ec2.RouteTableAssociation(
+                f"networking-private-route-table-association-{index}",
+                route_table_id=private_route_table,
+                subnet_id=subnet,
+                opts=pulumi.ResourceOptions(
+                    parent=private_route, providers=child_opts.providers
+                ),
             )
